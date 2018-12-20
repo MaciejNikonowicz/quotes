f53f5294ba2248eee5f9cfbac7f08143646d090c
Nikonowicz <maciej.nikonowicz@primesoft.pl>
2018-12-20 14:05:26 +0100

    0h - added CommentsController

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+    # create comment for quotation
+    def create
+        @quote = Quote.find(params[:quote_id])
+        @comment = @quote.comments.create(comment_params)
+        redirect_to quote_path(@quote)
+    end
+
+    def destroy
+        @quote = Quote.find(params[:quote_id])
+        @comment = @quote.comments.find(params[:id])
+        @comment.destroy
+        redirect_to quote_path(@quote)
+    end
+
+    private def comment_params
+        params.require(:comment).permit(:username, :body)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181220123457) do
+ActiveRecord::Schema.define(version: 20181220130424) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "quotes_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["quotes_id"], name: "index_comments_on_quotes_id"
+  end
 
   create_table "quotes", force: :cascade do |t|
     t.string   "title"
