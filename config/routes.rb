3d158b1ab3b578e01781cacfa54ced80e962ca56
Nikonowicz <maciej.nikonowicz@primesoft.pl>
2018-12-20 13:45:16 +0100

    0h - Created QuoteModel, edit, update, delete

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-    root 'quotes#index'
+    root 'quotes#index', as: 'home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
