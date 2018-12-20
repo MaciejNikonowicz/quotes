85c4276bf0d2493a376a48a33e2494bba63d4b57
Nikonowicz <maciej.nikonowicz@primesoft.pl>
2018-12-20 13:31:28 +0100

    0h - Created QuotesController, new quote form, bootstrap

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,11 @@
+class QuotesController < ApplicationController
+    # index method for listing all the quotes on main page
+    def index 
+    
+    end
+
+    # method for initializing new quote
+    def new
+        
+    end
+end
